77ae96000695485623c37d02ab89d4c08e5ed22c
Defman21 <thefyro1@gmail.com>
2016-05-01 23:16:37 +0300

    Misc: updated middleman-deploy dependency updated config.rb

--- config.rb
+++ config.rb
@@ -1,55 +1,26 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-page 'blog/index.html', layout: :layout
-page 'blog/*', layout: :blog
 page "index.html", layout: false
 page "materia.html", layout: false
-page "contact.html", layout: false
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-
-end
-
-activate :livereload
-
-activate :directory_indexes
+page 'blog/index.html', layout: :layout
+page 'blog/*', layout: :blog
 
 set :index_file, "index.html"
 set :markdown, fenced_code_blocks: true, smartypants: true, prettify: true
 set :markdown_engine, :redcarpet
 
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.build_before = true
+end
+
+activate :livereload
+activate :directory_indexes
 activate :ellipses
