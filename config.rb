3773dac5a0a3c8565c5654f0f0ba956d97e913b7
Defman21 <thefyro1@gmail.com>
2016-06-08 19:47:18 +0300

    Misc: Added page navigation

--- config.rb
+++ config.rb
@@ -9,6 +9,8 @@ page 'schemes/*', layout: :scheme
 page 'blog/index.html', layout: :layout
 page 'blog/*', layout: :blog
 
+ignore '/blog/pages/page.html'
+
 page '404.html', directory_index: false
 
 set :index_file, "index.html"
@@ -25,6 +27,42 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   deploy.build_before = true
+end
+
+ready do
+  $blogs = 5
+  blogs = sitemap.resources.select do |p|
+    if p.metadata[:options][:layout] == :blog and p.source_file.include? "md"
+      return true
+    end
+    return false
+  end
+  offset = 0
+  if blogs.length > $blogs
+    first, last = false, false
+    for page in 0..blogs.length.div($blogs)
+      _blogs = blogs[offset, $blogs]
+      _next_blogs = blogs[offset+$blogs, $blogs]
+      if _next_blogs.nil? or _next_blogs.length == 0
+        last = true
+      end
+      if offset == 0
+        first = true
+      else
+        first = false
+      end
+      proxy "/blog/pages/#{page+1}.html", "/blog/pages/page.html", locals: {
+        first: first,
+        last: last,
+        page: page+1,
+        blogs: _blogs
+        }, ignore: true
+      if last
+        break
+      end
+      offset += $blogs
+    end
+  end
 end
 
 activate :livereload, host: "dev.defman.me"
