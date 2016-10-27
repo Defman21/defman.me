db4643abbed437bdbbd381e63feadd542855a5c2
Defman21 <i@defman.me>
2016-10-27 23:58:17 +0300

    Automated commit at 2016-10-27 20:58:17 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/lazyload.js
+++ assets/javascripts/lazyload.js
@@ -1,1 +1,1 @@
-(function(){!function(){var n,t,e,o;return n=!1,o=document.getElementsByClassName("comments")[0],t=function(n){var t,e,o;return o=n.getBoundingClientRect(),e=o.top,t=o.bottom,e>0&&t<=window.innerHeight},e=function(){return t(o)&&!n&&(n=!0,"undefined"!=typeof loadDisqusComments&&null!==loadDisqusComments)?setTimeout(function(){return loadDisqusComments()},500):void 0},document.addEventListener("DOMContentLoaded",function(){return e()}),document.onscroll=function(){return e()}}()}).call(this);
+(function(){}).call(this);
